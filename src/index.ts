7b7d9eaa9645c6722f662e231ce58144d05173c9
Aron Hawkins <hawkins.aron@gmail.com>
2024-05-17 15:53:34 -0500

    add build file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import app from './api/config/express';
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4001;
 
 app.listen(PORT, () => console.log('Listening on port ' + PORT));
